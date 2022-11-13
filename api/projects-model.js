f1dcff70d8c47e8cb8325a0b096dea6c8c6b4207
unknown <macrowell@yahoo.com>
2022-11-13 10:28:25 -0500

    adds POST, PUT, and DELETE endpoints to projects router

--- api/projects-model.js
+++ api/projects-model.js
@@ -3,7 +3,24 @@ const db = require('../data/dbconfig.js')
 function get() {
     return db('projects')
 }
+function add(project) {
+    return db('projects')
+    .insert(project)
+}
+function update(id, changes) {
+    return db('projects')
+    .where("id", id)
+    .update(changes)
+}
+function remove(id) {
+    return db('projects')
+    .where("id", id)
+    .del()
+}
 
 module.exports = {
-    get
+    get,
+    add,
+    update,
+    remove
 }
--- api/projects-router.js
+++ api/projects-router.js
@@ -10,5 +10,27 @@ router.get('/', (req, res) => {
     })
     .catch(err => console.error(err))
 })
+router.post('/', (req, res) => {
+    Projects.add(req.body)
+    .then(() => {
+        res.status(200).json({ "message": "new project added successfully" })
+    })
+    .catch(err => console.error(err))
+})
+router.put('/:id', (req, res) => {
+    const { params, body } = req
+    Projects.update(params.id, body)
+    .then(() => {
+        res.status(200).json({ "message": "project has been updated successfully" })
+    })
+    .catch(err => console.error(err))
+})
+router.delete('/:id', (req, res) => {
+    Projects.remove(req.params.id)
+    .then(() => {
+        res.status(200).json({ "message": "project was deleted" })
+    })
+    .catch(err => console.error(err))
+})
 
 module.exports = router
